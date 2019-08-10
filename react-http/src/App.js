1d2866035e78ec83e2cd5d8da267746240b40dbd
shubham860 <shubham.chauhan860@gmail.com>
2019-08-10 16:54:11 +0530

    httpget

---
+++ react-http/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HTTPget from './Components/HTTPget'
+
+function App() {
+  return (
+    <div className="App">
+
+    <HTTPget/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-http/src/Components/HTTPget.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios'
+
+class HTTPget extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      list : [],
+      error : ''
+    }
+  }
+
+componentDidMount(){
+  axios.get('https://jsonplaceholder.typicode.com/posts')
+ .then(response=>{
+   console.log(response.data)
+   this.setState({list : response.data})
+ })
+.catch(error=>{
+  this.setState({error : 'error'})
+})
+}
+
+  render(){
+    const {list,error} = this.state
+    return(
+      <div>
+      list of elements
+      {
+      list.length?
+      list.map(list=><div key={list.id}>{list.title}</div>):null
+    }
+    {error?(<div>{error}</div>):null}
+    </div>
+  )
+  }
+}
+
+export default HTTPget;
